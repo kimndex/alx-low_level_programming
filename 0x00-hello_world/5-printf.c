80ac186949a713506476e6d889cd675e46170960
Nothando <kimndex@gmail.com>
2023-02-15 23:55:56 -0800

    betty style correction

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 
-/** Simple function
+/**
  * main - displays a statement
+ * Return: 0
  */
-int main(void)
+int main (void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return(0);
