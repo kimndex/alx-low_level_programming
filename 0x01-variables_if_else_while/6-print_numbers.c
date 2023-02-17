efc640aa241d5a288a1b22bedd6823dd36ce5f01
Nothando <kimndex@gmail.com>
2023-02-17 02:32:01 -0800

    numbers

--- 0x01-variables_if_else_while/6-print_numbers.c
+++ 0x01-variables_if_else_while/6-print_numbers.c
@@ -2,6 +2,9 @@
 /**
  * main - main block
  * Description: Print all numbers of base 10, starting from 0.
+ * You are not allowed to use any variable of type char.
+ * You can only use `putchar` to print to console.
+ * You can only use `putchar` twice.
  * Return: 0
  */
 int main(void)
@@ -10,7 +13,7 @@ int main(void)
 
 	for (i = 0; i < 10; i++)
 	{
-		printf("%i", i);
+		putchar(i + '0');
 	}
 	putchar('\n');
 
