22c45f19d42964913bb85a2de68b03958454e128
Nothando <kimndex@gmail.com>
2023-03-06 08:12:38 -0800

    memset

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,10 +1,13 @@
 #include "main.h"
 
 /**
- * main - main function
- * Description: copies memory area
- * Return: pointer to dest
+ * _memcpy - copy memory area
+ * @dest: char array to copy into
+ * @src: char array to copy from
+ * @n: number of elements to copy
+ * Return: pointer to `dest`
  */
+
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
